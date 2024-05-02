f5e3d095a1b0399c7e4c6812c407e45617b5fe40
Igbagbo <iolufiranye@gmail.com>
2024-05-02 20:05:13 +0100

    second commit

---
+++ src/components/CallToAction.jsx
@@ -0,0 +1,11 @@
+import React, {useEffect, useRef} from "react" 
+import { motion, useInView, useAnimation } from "framer-motion"
+
+const CallToAction = () => {
+    const {innerWidth}
+  return (
+    <div>CallToAction</div>
+  )
+}
+
+export default CallToAction
